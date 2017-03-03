e0af9faedc472543f7337b2092fad8e5dc649697
Julien Goldberg <julien@catalisio.com>
2017-03-03 14:29:26 +0100

    remove ?

--- src/Client.php
+++ src/Client.php
@@ -3,7 +3,7 @@
  * @Author: catalisio
  * @Date:   2016-02-27 16:54:30
  * @Last Modified by:   Julien Goldberg
- * @Last Modified time: 2017-03-03 14:22:14
+ * @Last Modified time: 2017-03-03 14:29:16
  */
 
 namespace Catalisio\APIClient;
@@ -121,7 +121,7 @@ abstract class Client
 		
 		if (isset($queryParams) && count($queryParams) > 0)	{
 
-			$queryString = implode('&', array_map(
+			$queryString = '?' . implode('&', array_map(
    														function ($v, $k) { return $k . '=' . $v; }, 
     													$queryParams, 
     													array_keys($queryParams)
@@ -132,6 +132,6 @@ abstract class Client
 			$queryString = '';
 		}
 
-		return sprintf("%s%s?%s", $this->getEndPoint(), $url, $queryString);
+		return sprintf("%s%s%s", $this->getEndPoint(), $url, $queryString);
 	}
 }
